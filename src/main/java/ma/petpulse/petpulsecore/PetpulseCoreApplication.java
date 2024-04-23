e695190e7ee8ec5736d5c954c368aa7062f616ea
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-04-23 23:00:00 +0100

    init

---
+++ src/main/java/ma/petpulse/petpulsecore/PetpulseCoreApplication.java
@@ -0,0 +1,13 @@
+package ma.petpulse.petpulsecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetpulseCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetpulseCoreApplication.class, args);
+    }
+
+}
