6ad6eeedf909565ea53e97eae62556e046a89ac4
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-04-27 16:09:12 +0100

    Updated Pet Entity

--- src/main/java/ma/petpulse/petpulsecore/dao/entities/Pet.java
+++ src/main/java/ma/petpulse/petpulsecore/dao/entities/Pet.java
@@ -1,15 +1,34 @@
 package ma.petpulse.petpulsecore.dao.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "pets")
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Pet {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Breed is required")
+    @Size(max = 100, message = "Breed cannot exceed 100 characters")
+    private String breed;
+
+    @Min(value = 0, message = "Age must be greater than or equal to 0")
+    private int age;
+
     @OneToMany(mappedBy = "pet", cascade = CascadeType.ALL)
     private List<Report> reports = new ArrayList<>();
 
