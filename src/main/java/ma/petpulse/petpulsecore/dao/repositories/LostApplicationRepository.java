38123cc4353725f4373acf5ac0b8c57fe063382f
zendour younes <y.zendour2@gmail.com>
2024-05-17 11:49:24 +0100

    lost applications service

--- src/main/java/ma/petpulse/petpulsecore/dao/repositories/LostApplicationRepository.java
+++ src/main/java/ma/petpulse/petpulsecore/dao/repositories/LostApplicationRepository.java
@@ -1,8 +1,14 @@
 package ma.petpulse.petpulsecore.dao.repositories;
 
+import ma.petpulse.petpulsecore.dao.entities.AdoptionApplication;
 import ma.petpulse.petpulsecore.dao.entities.LostApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LostApplicationRepository extends JpaRepository<LostApplication, Long>{
+import javax.swing.*;
+import java.util.List;
 
+public interface LostApplicationRepository extends JpaRepository<LostApplication, Long> {
+    List<LostApplication> findByUserId(Long userId);
+
+    List<LostApplication> findByReportId(Long reportId);
 }
